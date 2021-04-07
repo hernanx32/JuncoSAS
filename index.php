29700a8c093f13bdbe8a9de41f84ad0fd76bb8f5
Ayala Hernan <webfsa@gmail.com>
2021-04-07 08:14:49 -0300

    a

--- index.php
+++ index.php
@@ -1,13 +1,23 @@
 <?PHP session_start();
 require_once("funciones.php");
-$titulopag='INICIO';
+$titulopag='Inicio de Sesión';
 
-cabezera($titulopag, $datos_nombempresa);	
-
+$scr=$_GET[scr];
 if($scr=='logout'){
 	session_destroy();
+	echo 'Usuario Deslogeado';
+	
+}elseif ($scr=='logeo')
+{ 
+$usuario = $_POST['usuario'];
+$clave = $_POST['clave'];
+$usuario = stripslashes($usuario);
+$clave = stripslashes($clave);
+	echo 'Logeando Usuario'.$usuario;
+}else {
+	
+	echo 'Inicio Normal';
+	
 }
-
-
-
+cabezera($titulopag, $datos_nombempresa);	
 ?>
