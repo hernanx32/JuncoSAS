7f926e7b50c2339ebc8bd2bd73daff4fd94e45ee
Hernan Ayala <Hernan_Ayala@apa.corp>
2021-04-09 21:21:06 -0300

    123

--- index.php
+++ index.php
@@ -7,17 +7,15 @@ if($scr=='logout'){
 	session_destroy();
 	echo 'Usuario Deslogeado';
 	
-}elseif ($scr=='logeo')
-{ 
-$usuario = $_POST['usuario'];
-$clave = $_POST['clave'];
-$usuario = stripslashes($usuario);
-$clave = stripslashes($clave);
+}elseif ($scr=='logeo'){ 
+	
+$usuario = $_POST['usuario'];   	$clave = $_POST['clave'];
+$usuario = stripslashes($usuario);  $clave = stripslashes($clave);
+	
 	echo 'Logeando Usuario'.$usuario;
+	
 }else {
-	
 	echo 'Inicio Normal';
-	
 }
 cabezera($titulopag, $datos_nombempresa);	
 ?>
