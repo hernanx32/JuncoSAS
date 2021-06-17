a045113307f0d67c4b7e41d5b9f0ab479351bf5f
Hernan Ayala <Hernan_Ayala@apa.corp>
2021-06-17 17:24:42 -0300

    17-06-21-01

--- PRUEBAS/menu.php
+++ PRUEBAS/menu.php
@@ -4,21 +4,28 @@
 <meta charset="utf-8">
 	
 <title>Documento sin título</title>
+<link href="/Junco/PRUEBAS/css/menu.css" rel="stylesheet" type="text/css" media="screen">
 </head>
 <body>
-	
-<link rel="stylesheet" href="css/menu.css">
+
 <nav>
 	<ul>
-		<li><a href="Principal.php">Inicio</a></li>
-		<li><a href="#">Servicios</a>
+		<li><a href="#">M1</a>
+		<div>
+			<ul>
+			<li class="titulo"><a href="">SMTITULO</a></li>
+			<li><a href="">SM1</a></li>
+			<li><a href="">SM2</a></li>
+			<li><a href="">SM3</a></li>
+			</ul>	</li>
+		</div>	
+
+		<li><a href="#">M2</a>
 			<div>
 				<ul>
-					<li class="titulo"><a href="">Categoria #1</a></li>
-					<li><a href="">Categoria #2</a></li>
-					<li><a href="">Categoria #3</a></li>
-					<li><a href="">Categoria #4</a></li>
-					<li><a href="">Categoria #5</a></li>
+					<li><a href="">SM4</a></li>
+					<li><a href="">SM5</a></li>
+					<li><a href="">SM6</a></li>
 				</ul>
 				<ul>
 					<li class="titulo azul"><a href="">Categoria #1</a></li>
@@ -27,34 +34,34 @@
 					<li><a href="">Categoria #4</a></li>
 					<li><a href="">Categoria #5</a></li>
 				</ul>
-
-					<ul>
-						<li class="titulo verde"><a href="">Categoria #1</a></li>
-						<li><a href="">Categoria #2</a></li>
-						<li><a href="">Categoria #3</a></li>
-						<li><a href="">Categoria #4</a></li>
-						<li><a href="">Categoria #5</a></li>
-					</ul>
-					<ul>
-						<li class="titulo rojo"><a href="">Categoria #1</a></li>
-						<li><a href="">Categoria #2</a></li>
-						<li><a href="">Categoria #3</a></li>
-						<li><a href="">Categoria #4</a></li>
-						<li><a href="">Categoria #5</a></li>
-					</ul>
-					<ul>
-						<li class="titulo naranja"><a href="">Categoria #1</a></li>
-						<li><a href="">Categoria #2</a></li>
-						<li><a href="">Categoria #3</a></li>
-						<li><a href="">Categoria #4</a></li>
-						<li><a href="">Categoria #5</a></li>
-					</ul>
-				</div>
-				</li>
-				<li><a href="#">Productos</a></li>
-				<li><a href="#">Acerca de</a></li>
-				<li><a href="#">Contacto</a></li>
-			</ul>
-		</nav>
+				<ul>
+					<li class="titulo verde"><a href="">Categoria #1</a></li>
+					<li><a href="">Categoria #2</a></li>
+					<li><a href="">Categoria #3</a></li>
+					<li><a href="">Categoria #4</a></li>
+					<li><a href="">Categoria #5</a></li>
+				</ul>
+				<ul>
+					<li class="titulo rojo"><a href="">Categoria #1</a></li>
+					<li><a href="">Categoria #2</a></li>
+					<li><a href="">Categoria #3</a></li>
+					<li><a href="">Categoria #4</a></li>
+					<li><a href="">Categoria #5</a></li>
+				</ul>
+				<ul>
+					<li class="titulo naranja"><a href="">Categoria #1</a></li>
+					<li><a href="">Categoria #2</a></li>
+					<li><a href="">Categoria #3</a></li>
+					<li><a href="">Categoria #4</a></li>
+					<li><a href="">Categoria #5</a></li>
+				</ul>
+			</div>
+			</li>
+			<li><a href="#">Productos</a></li>
+			<li><a href="#">Acerca de</a></li>
+			<li><a href="#">Contacto</a></li>
+		</ul>
+	</nav>
+	
 	</body>
 </html>
