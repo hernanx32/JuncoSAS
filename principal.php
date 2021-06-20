97f20a779a593877e0c0040abbdfd9fc929ee020
Hernan Ayala <Hernan_Ayala@apa.corp>
2021-06-20 08:26:59 -0300

    3622622

--- principal.php
+++ principal.php
@@ -10,7 +10,30 @@ if($_SESSION['rol']!='1'){
 
 echo "<h1>Bienvenido: " .$_SESSION['nom_vista']."</h1>";
 ?>
+<link href="/Junco/css/menudesp.css" rel="stylesheet" type="text/css" media="screen">
 
-</br>
-<p><a href="/Junco/index.php?scr=logout">Salir</a><span style="color: #000000"></span></p>
-<p>&nbsp;</p>
+
+<div id="header">
+	<ul class="nav">
+	<li><a href="#">Inicio</a></li>
+	<li><a href="#">Archivos</a>
+		<ul>
+			<li><a href="#">Claves</a></li>
+			<li><a href="#">Mensajes</a></li>
+		</ul>
+		</li>
+	<li><a href="#">Comprobantes</a>
+		<ul>
+			<li><a href="#">Proformas</a></li>
+			<li><a href="#">Presupuestos</a></li>
+			<li><a href="#">Otros</a>
+				<ul>
+					<li><a href="#">Sec1</a></li>
+					<li><a href="/Junco/index.php">Sec2</a></li>
+				</ul>	
+			</li>
+		</ul>
+		</li>
+	<li><a href="index.php?scr=logout">Salir</a></li>
+	</ul>
+</div>
